b8a200ca993977ab626a041d0c1e10b361efdc54
Lee Chastain <leechastain@gamestop.com>
2013-12-25 07:46:05 -0600

    Initial Test class shell

---
+++ tests/LaffingasTest.java
@@ -0,0 +1,65 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LaffingasTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testActionPerformed() throws Exception {
+
+    }
+
+    @Test
+    public void testClear() throws Exception {
+
+    }
+
+    @Test
+    public void testCheckComplete() throws Exception {
+
+    }
+
+    @Test
+    public void testFindNext() throws Exception {
+
+    }
+
+    @Test
+    public void testFindHighlander() throws Exception {
+
+    }
+
+    @Test
+    public void testFindX() throws Exception {
+
+    }
+
+    @Test
+    public void testHandleMenuBar() throws Exception {
+
+    }
+
+    @Test
+    public void testHighlightOff() throws Exception {
+
+    }
+
+    @Test
+    public void testHighlightOn() throws Exception {
+
+    }
+
+    @Test
+    public void testSweep() throws Exception {
+
+    }
+}
